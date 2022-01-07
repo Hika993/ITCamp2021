8dcc9d43963a901fbfae449f3105d3b788387afd
unknown <hikadn993@gmail.com>
2022-01-07 19:58:34 +0100

    js

---
+++ js5/app.js
@@ -0,0 +1,67 @@
+
+// pocetni = parseInt(prompt("Pocetni unos"))
+
+// krajni = parseInt(prompt("Krajni unos"))
+
+// for(i = pocetni; i<=krajni; i ++ ){
+
+//     console.log(i**3);
+
+
+// 
+
+
+
+// pocetni = parseInt(prompt("Pocetni unos"))
+
+// krajni = parseInt(prompt("Krajni unos"))
+
+// for(i = pocetni; i<=krajni; i ++ ){
+
+//     delioci = 0;
+
+//     for ( j=1; j<=i; j++) {
+//         if ( i % j === 0){ delioci ++;}
+//     }
+// if (delioci === 2 ) {
+//     console.log(i);
+// }
+
+// 
+
+// pocetni = parseInt(prompt("Pocetni unos"))
+
+// krajni = parseInt(prompt("Krajni unos"))
+
+// for(i = pocetni; i<=9000; i ++ ){
+
+//     if ( i % pocetni === 0 && i % krajni === 0) {
+//         console.log(i);
+//         break;
+//     }
+
+
+// }
+
+// kolicina=parseInt(prompt("uNESITE Kolicinu"))
+// sum="";
+// for (i = 0 ; i <kolicina; i++) {
+//     unos = prompt ("Unesite slovo")
+//     sum += unos;
+// }
+
+// console.log(sum);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
