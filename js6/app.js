68825547c1b8e681d99ee187b72c524e45911d64
unknown <hikadn993@gmail.com>
2022-01-10 19:54:49 +0100

    js

---
+++ js6/app.js
@@ -0,0 +1,48 @@
+// a = 6;
+
+// for ( let i = 1; i <= a ; i++) {
+//     console.log(i);
+
+// }
+
+
+// var1=5;
+// fakt=1;
+
+// for (
+//     let i = 1; i <= 5; i++) 
+//     {
+//         fakt= fakt * i;
+
+//         console.log(fakt);
+//     }
+//     console.log(fakt);
+
+
+// a=prompt("Unesite broj:"); 
+
+// sumfakt=0;
+// for ( let i = 1; i <=a; i++)
+
+// {
+//     fakt=1;
+
+//     for ( let j=1; j<=i; j ++) {
+//         fakt = fakt * j;
+//     }
+//     console.log(fakt);
+//     sumfakt += fakt;
+// }
+// console.log(sumfakt);
+
+// for ( let i=1; i <=10; i++)
+// {red ="";
+
+
+// for (let j = 1; j <=10; j++)
+// {
+    
+//  red += `${j * i} \t`
+// }
+// console.log(red); }
+
