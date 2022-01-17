0f4f595c5504d0db19fd37cd10ed107fb7f3674a
unknown <hikadn993@gmail.com>
2022-01-17 19:49:52 +0100

    js

---
+++ js6/app1.js
@@ -0,0 +1,66 @@
+
+
+
+//   tezina = parseInt(prompt("Unesite tezinu"));
+//   visina = parseInt(prompt("Unesite visinu"));
+
+//   bmi = tezina/visina ** 2;
+
+//   if (bmi<18.5) {
+//       console.log("Underweight");
+//   } else if (bmi < 25) {
+//       console.log("nORMALN");
+//   } else if (bmi < 30) {
+//       console.log("Overweight");
+//   } else if (bmi >30) {
+//       console.log("Obese");
+//   }
+
+
+// n = parseInt(prompt("Unesi krajnji broj"));
+// brojac = 1;
+
+// while ( brojac <= n) {
+//     if (brojac%2 !==0) {
+//         console.log(brojac);
+//     }
+//     brojac++
+// }
+
+// unos = prompt ("Unesite broj");
+
+// while (unos !=="STOP") {
+//     if (unos ==0) {
+//         console.log("ZERO");
+//     } else if (unos > 0) {
+//         console.log("POZITIVAN");
+//     } else if ( unos < 0) {
+//         console.log("NEGATIVAN");
+//     }
+//     unos = prompt ("Unesite nesto opet");
+// }
+
+// for ( i = 0; i < 2050; i+=4   ) {
+//     console.log(i);
+// }
+
+// pocetna = prompt("Unesi pocetnu vrednost");
+// krajna = prompt("Unesi krajnju vrednost");
+
+// for (i=pocetna; i<=krajna; i++){
+//     if ( i % 4 === 0) {
+//         console.log(i);
+//     }
+// }
+
+
+// broj  = parseInt(prompt("Unesi broj"));
+// brojac = 1;
+// fact = 1;
+
+// while (brojac <= broj) {
+//     fact *= brojac;
+//     brojac++
+
+// }
+// console.log(fact);
