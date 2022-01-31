757e42c49581e7cbf20cdef32a9822f28bdb815b
unknown <hikadn993@gmail.com>
2022-01-31 12:37:06 +0100

    js

--- js6/app1.js
+++ js6/app1.js
@@ -149,3 +149,127 @@
 
 // console.log(broj);
 
+
+// var a = ("Ocena je: " +5);
+// var b = (5 + " je ocena");
+// var hi = ("Hello World");
+// console.log(a);
+// console.log(b);
+// console.log(hi);
+
+// a = 5
+// b = 3
+// c = a+b
+// console.log(c);
+
+// a = [3, 6, 23, 9, 44, 14, 83, 22, 5, 1];
+
+// sum = 0;
+
+// for (i=0; i<a.length; i++) {
+//         sum+= a[i];
+// }
+// console.log(sum / a.length)
+
+// arr = [3, 6, 0, 23, 0, 9, 44, 14, 0, 83, 22, 5, 0, 1];
+
+// for (i=0; i<arr.length; i++) {
+//     if (arr[i]===0) {
+//         arr.splice(i , 1);
+//         arr.push(0);
+//     }
+// }
+// console.log(arr);
+
+
+// arr = [3, 6, 23, 9, 44, 14, 0, 83, 22, 5,];
+// min = arr[0];
+// max = arr [0];
+// for (el of arr) {
+//     if (el>max) {
+//         max=el;
+//     }
+// else if (min > el) {
+//     min = el;
+// }
+// }
+
+// console.log("Razlika je:", max - min);
+
+
+// arr = [3, 6, 23, 9, 44, 14, 0, 83, 22, 5,];
+
+// sadrzi = false;
+
+// for ( el of arr) {
+//     if (el===67||el===87) {
+//         sadrzi=true;
+//     }
+// }
+
+// console.log(sadrzi);
+
+
+// arr = [3, 6, 23, 9, 44, 14, 0, 83, -254, 22, 5,];
+// max = arr[0];
+
+// for (el of arr) {
+//     if(Math.abs(el)>Math.abs(max)) {
+//     max=el;   }
+// }
+
+// console.log(max);
+
+
+// arr = [3, 6, 23, 9, 44, 14, 0, 83, -254, 22, 5,];
+// min1= arr[0];
+// min2= arr[0];
+// for (el of arr) {
+//     if (el <min1) {
+//         min1=el;
+//     } else if (el<min2 && el>min1) {
+//         min2=el;
+//     }
+// }
+
+// console.log("Najmanji je:", min1);
+// console.log("Drugi manje je:", min2);
+
+
+// data = [[1, 2, 3] , 2];
+// console.log(data[0][2]);
+
+
+
+// function sum() {
+//     a = parseInt( prompt("Unesi prvi broj"));
+//     b = parseInt( prompt("Unesi drugi broj"));
+//        console.log("Zbir ova dva broja je:" , a+b);
+
+// }
+
+// sum();
+
+// function sayHello(ime){
+//     console.log("HELLO" , ime);
+// }
+
+// sayHello("Hika");
+// sayHello("Hika");
+// sayHello("Hika");
+
+
+// function zbir(a,b){
+//     console.log(a+b);
+// }
+
+// zbir(3,5);
+
+
+function mojaF(){
+    console.log("asd");
+    console.log("assd");
+    return [1, 2, 3,];
+}
+
+console.log(mojaF());
